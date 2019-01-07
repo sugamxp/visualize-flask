d56c4367ec8f4649425e1249f14b3616369ffdc0
sugamxp <sugamxp@gmail.com>
2019-01-07 21:12:11 +0530

    ext check

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,flash
 from forms import FileForm
 from werkzeug.utils import secure_filename
 import os
+import pandas as pd
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'waduhek'
+ALLOWED_EXT = {'csv','tsv', 'xlsx'}
 @app.route('/',methods=['GET','POST'])
 def index():
     form = FileForm()
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
         print(filename)
-        form.file.data.save('uploads/' + filename)
-        return redirect(url_for('index'))
+        if filename.split('.')[1] in ALLOWED_EXT:
+            form.file.data.save('uploads/' + filename)
+        else:
+            flash('Incorrect format selected, please try again!')
+        return redirect(url_for('pand'))
     return render_template('index.html', form=form)
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -3,5 +3,5 @@ from flask_wtf.file import FileField, FileRequired
 from wtforms import SubmitField, validators
 
 class FileForm(FlaskForm):
-    file = FileField("Select csv/tsv/xlsx file : ", validators=[FileRequired()], )
+    file = FileField("Select csv/tsv/xlsx file : ", validators=[FileRequired()] )
     submit = SubmitField('Upload file')
